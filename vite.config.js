1da24277f3730be4d1902b068b2b2529da326485
hDanford <hdanford01@gmail.com>
2026-03-03 15:02:42 -0500

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // When deploying to GitHub Pages, set base to your repo name:
-  // base: '/your-repo-name/'
-  base: '/',
+  base: '/sleeved/',
 });
